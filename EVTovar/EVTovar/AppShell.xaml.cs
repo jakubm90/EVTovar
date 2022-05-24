d8a2491962e2f667d6f8d6476444d13bae467f02
jakubm90 <88741857+jakubm90@users.noreply.github.com>
2022-05-24 10:26:31 +0200

    Add project files.

---
+++ EVTovar/EVTovar/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using EVTovar.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace EVTovar
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
